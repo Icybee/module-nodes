d62ab7be7c4253e01d9ca34f5f2da189899235a1
Olivier Laviale <olivier.laviale@gmail.com>
2015-03-22 19:49:14 +0100

    Added UsernameCriterion

---
+++ lib/activerecords/facets/username.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the Icybee package.
+ *
+ * (c) Olivier Laviale <olivier.laviale@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Icybee\Modules\Nodes;
+
+use ICanBoogie\ActiveRecord\Query;
+use ICanBoogie\Facets\Criterion;
+
+class UsernameCriterion extends Criterion
+{
+	public function alter_query(Query $query)
+	{
+		/*
+		$username_query = $query->model->models['users']
+			->select('uid, username');
+
+		$query->join($username_query, [ 'as' => 'username' ]);
+
+		TODO: Query tries content.uid = username.uid, but `uid` is only available in node
+
+		*/
+
+		$query->join('INNER JOIN (SELECT uid, username FROM {prefix}users) AS username ON (node.uid = username.uid)');
+
+		return $query;
+	}
+}
